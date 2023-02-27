7dc0f48244dea3e91dd93b4f8f7e0a232bd05287
Replit user <>
2023-02-27 16:49:31 +0000

    Add style

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Abuja',
+    'salary': 'Rs. 12,00,000'
+  },
+   {
+    'id': 2,
+    'title': 'Frontend Engineer',
+    'location': 'America',
+    'salary': 'Rs. 14,00,000'
+  },
+   {
+    'id': 3,
+    'title': 'Data Scientist',
+    'location': 'San Francisco, USA',
+    'salary': 'Rs. 18,00,000'
+  },
+   {
+    'id': 1,
+    'title': 'Backend Engineer',
+    'location': 'Remote',
+    'salary': 'Rs. 12,00,000'
+  },
+]
+
 @app.route("/")
 def hello_world():
-  return "Hello, america!"
+  return render_template("home.html", jobs=JOBS,
+                        company_name="Joviers")
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
